edaa9321ef4f8cf024547466b4087be389b38bc4
Lun-Ching Chang <lunching@gmail.com>
2018-01-19 10:51:08 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,20 @@
+## Check packages ##
+if (!require('DT')) install.packages("DT")
+if (!require('shiny')) install.packages("shiny") 
+if (!require('shinythemes')) install.packages("shinythemes") 
+if (!require('shinyFiles')) install.packages("shinyFiles") 
+
+if (!require('DNAcopy')){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("DNAcopy")
+}
+ 
+if (!require('Rsamtools')){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("Rsamtools")
+}
+
+## R functions ##
 source("functions/data_preproc.R")
 source("functions/HCMM_CNVs.R")
 source("functions/plot_HCMMCNVs.R")
