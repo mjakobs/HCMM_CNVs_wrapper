5c26a1ba356df8ab24f88f3bd2a5edccc6a86b45
mjakobs <25904555+mjakobs@users.noreply.github.com>
2021-11-24 16:32:14 +0000

    print paste progress for HCMM_CNVs

--- functions/HCMM_CNVs.R
+++ functions/HCMM_CNVs.R
@@ -6,7 +6,8 @@ HCMM_CNVs<- function(Cov_matrix, n_cluster, bed_file_sorted, sample_names, filen
     h_all<- c()
     n_cluster_all<- c()
     for(i in 1: ncol(Cov_matrix)){
-      progress$inc((1/ncol(Cov_matrix))*0.4 , detail = paste("Running Hierarchical Clustering for region ", i, sep=""))
+      #progress$inc((1/ncol(Cov_matrix))*0.4 , detail = paste("Running Hierarchical Clustering for region ", i, sep=""))
+      print(paste("Running Hierarchical Clustering for region ", i, sep=""))
       hc_tmp<- hclust(dist(Cov_matrix[,i]/median(Cov_matrix[,i])))
       h_tmp<- 0
       n_cluster_tmp <- length(unique(cutree(hc_tmp, h = h_tmp)))
@@ -34,7 +35,8 @@ HCMM_CNVs<- function(Cov_matrix, n_cluster, bed_file_sorted, sample_names, filen
     CBS_all<- list()
     
     for(i in 1: ncol(matrix_adj_Cov_hc_log2_ratio)){
-      progress$inc((1/ncol(matrix_adj_Cov_hc_log2_ratio))*0.4 , detail = paste("Running CBS for sample ", i, sep=""))
+      #progress$inc((1/ncol(matrix_adj_Cov_hc_log2_ratio))*0.4 , detail = paste("Running CBS for sample ", i, sep=""))
+      print(paste("Running CBS for sample ", i, sep=""))
       log_ratio_adj_cov_hc_tmp<- matrix_adj_Cov_hc_log2_ratio[, i]
       CNA_data_hc_tmp<- CNA(log_ratio_adj_cov_hc_tmp, chrom = bed_file_sorted[,1], maploc = maploc_center_tmp, data.type = "logratio", sampleid = sample_names[i])
       smoothed_CNA_hc_object <- smooth.CNA(CNA_data_hc_tmp)
@@ -61,7 +63,8 @@ HCMM_CNVs<- function(Cov_matrix, n_cluster, bed_file_sorted, sample_names, filen
       CBS_all[[i]]$output[,"seg.mean"] <- means.new
     }
     
-    progress$inc(0.1, detail = paste("Saving result into RData"))
+    #progress$inc(0.1, detail = paste("Saving result into RData"))
+    print( paste("Saving result into RData"))
     names(CBS_all) <- sample_names
     save(CBS_all, file = paste("CBS_", filename, ".RData", sep=""))
     test<- paste("There are ", ncol(Cov_matrix), " target regions.", sep="")
@@ -74,7 +77,8 @@ HCMM_CNVs<- function(Cov_matrix, n_cluster, bed_file_sorted, sample_names, filen
     Cov_matrix <- Cov_matrix*adj_tmp
     adj_tmp <- 2/as.numeric(Ps_est[,2])
     for(i in 1: ncol(Cov_matrix)){
-      progress$inc((1/ncol(Cov_matrix))*0.4 , detail = paste("Running Hierarchical Clustering for region ", i, sep=""))
+      #progress$inc((1/ncol(Cov_matrix))*0.4 , detail = paste("Running Hierarchical Clustering for region ", i, sep=""))
+      print(paste("Running Hierarchical Clustering for region ", i, sep=""))
       hc_tmp<- hclust(dist(Cov_matrix[,i]/median(Cov_matrix[,i])))
       h_tmp<- 0
       n_cluster_tmp <- length(unique(cutree(hc_tmp, h = h_tmp)))
@@ -102,7 +106,8 @@ HCMM_CNVs<- function(Cov_matrix, n_cluster, bed_file_sorted, sample_names, filen
     CBS_all<- list()
     
     for(i in 1: ncol(matrix_adj_Cov_hc_log2_ratio)){
-      progress$inc((1/ncol(matrix_adj_Cov_hc_log2_ratio))*0.4 , detail = paste("Running CBS for sample ", i, sep=""))
+      #progress$inc((1/ncol(matrix_adj_Cov_hc_log2_ratio))*0.4 , detail = paste("Running CBS for sample ", i, sep=""))
+      print(paste("Running CBS for sample ", i, sep=""))
       log_ratio_adj_cov_hc_tmp<- matrix_adj_Cov_hc_log2_ratio[, i]
       CNA_data_hc_tmp<- CNA(log_ratio_adj_cov_hc_tmp, chrom = bed_file_sorted[,1], maploc = maploc_center_tmp, data.type = "logratio", sampleid = sample_names[i])
       smoothed_CNA_hc_object <- smooth.CNA(CNA_data_hc_tmp)
@@ -129,7 +134,8 @@ HCMM_CNVs<- function(Cov_matrix, n_cluster, bed_file_sorted, sample_names, filen
       CBS_all[[i]]$output[,"seg.mean"] <- means.new
     }
     
-    progress$inc(0.1, detail = paste("Saving result into RData"))
+    #progress$inc(0.1, detail = paste("Saving result into RData"))
+    print(paste("Saving result into RData"))
     names(CBS_all) <- sample_names
     save(CBS_all, file = paste("CBS_", filename, ".RData", sep=""))
     test<- paste("There are ", ncol(Cov_matrix), " target regions.", sep="")
