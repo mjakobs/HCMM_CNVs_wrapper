16f0b31ed88b48839176b2e89c54492bcd1f70d1
mjakobs <25904555+mjakobs@users.noreply.github.com>
2021-11-24 16:29:49 +0000

    add ploidy option

--- HCMM_CNVs_wrapper.R
+++ HCMM_CNVs_wrapper.R
@@ -18,6 +18,7 @@ bam_dir <- vars[4]
 min_cov <- vars[5]
 filename <- vars[6]
 n_cluster <- vars[7]
+ploidy_path <- vars[8]
 
 #set working directory to code directory so you can source the functions
 source(paste(code_dir,"functions/data_preproc.R", sep = ""))
@@ -30,3 +31,20 @@ source(paste(code_dir,"functions/plot_HCMMCNVs.R", sep = ""))
 # Run pre-processing #
 ######################
 data_preproc(bed_file = bed_file, chr = chr, bam_dir = bam_dir, min_cov = min_cov, filename = filename)
+
+print("Finished pre-processing!")
+
+######################
+#    Run HCMM-CNVs   #
+######################
+
+ploidy_option <- NA
+if(paste(ploidy_path) == "1"){
+  ploidy_option <- "ploidy = \"1\""
+}else{
+  ploidy_option <- "ploidy_path = ploidy_path"
+}
+
+HCMM_CNVs(Cov_matrix = matrix_adj_Cov_rm_duplicated, n_cluster = n_cluster, 
+          bed_file_sorted = bed_file_sorted, sample_names = sample_names, 
+          filename = filename, paste(ploidy_option))
