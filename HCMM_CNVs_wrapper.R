65f3836b1e875263ef4f49b2f9310f0c730a11ae
mjakobs <25904555+mjakobs@users.noreply.github.com>
2021-11-24 16:01:02 +0000

    started editing to work with jobscript command line arguments

--- HCMM_CNVs_wrapper.R
+++ HCMM_CNVs_wrapper.R
@@ -1,8 +1,4 @@
 # Rscript to run HCMM-CNVs from the command line
-source("functions/data_preproc.R")
-source("functions/HCMM_CNVs.R")
-source("functions/Mixture_Model.R")
-source("functions/plot_HCMMCNVs.R")
 
 # expected input preproc: bed_file, chr, bam_dir, min_cov, filename -- removed progress
 # expected output preproc: sample_names, bed_file_sorted, matrix_adj_Cov_rm_duplicated
@@ -12,16 +8,23 @@ source("functions/plot_HCMMCNVs.R")
 
 # expected input plot_HCMMCNVs:result, sample_id
 
-vars.tmp <- commandArgs()
-vars <- vars.tmp[length(vars.tmp)]
-split.vars <- unlist(strsplit(vars," "))
-###  Setting input paths for normalized read count and experimental design ###
-bed_file <- split.vars[1]
-chr <- split.vars[2]
-bam_dir <- split.vars[3]
-min_cov <- split.vars[4]
-filename <- split.vars[5]
-n_cluster <- split.vars[6]
+# getting command line arguments
+vars <- commandArgs(trailingOnly=TRUE)
+###  Setting input variables from jobscript ###
+code_dir <- vars[1]
+bed_file <- vars[2]
+chr <- vars[3]
+bam_dir <- vars[4]
+min_cov <- vars[5]
+filename <- vars[6]
+n_cluster <- vars[7]
+
+#set working directory to code directory so you can source the functions
+source(paste(code_dir,"functions/data_preproc.R", sep = ""))
+source(paste(code_dir,"functions/HCMM_CNVs.R", sep = ""))
+source(paste(code_dir,"functions/Mixture_Model.R", sep = ""))
+source(paste(code_dir,"functions/plot_HCMMCNVs.R", sep = ""))
+
 
 ######################
 # Run pre-processing #
