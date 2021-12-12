94eb7e95a7b2f55e178e7b901b426c5458295b81
mjakobs <25904555+mjakobs@users.noreply.github.com>
2021-12-12 19:14:34 +0000

    split analysis based on whether we want to analyse one or all chromosomes

--- HCMM_CNVs_wrapper.R
+++ HCMM_CNVs_wrapper.R
@@ -29,41 +29,93 @@ source(paste(code_dir,"functions/HCMM_CNVs.R", sep = ""))
 source(paste(code_dir,"functions/Mixture_Model.R", sep = ""))
 source(paste(code_dir,"functions/plot_HCMMCNVs.R", sep = ""))
 
-
-######################
-# Run pre-processing #
-######################
-data_preproc(bed_file = bed_file, chr = chr, bam_dir = bam_dir, min_cov = min_cov, filename = filename)
-
-print("Finished pre-processing!")
-
-######################
-#    Run HCMM-CNVs   #
-######################
-
-ploidy_option <- NA
-if(paste(ploidy_path) == "1"){
-  ploidy_option <- "ploidy = \"1\""
-}else{
-  ploidy_option <- "ploidy_path = ploidy_path"
+if(chr != "all"){
+  print("Running single chromosome analysis")
+  chr_tmp <- chr
+  ######################
+  # Run pre-processing #
+  ######################
+  data_preproc(bed_file = bed_file, chr = chr_tmp, bam_dir = bam_dir, min_cov = min_cov, filename = filename)
+  
+  print("Finished pre-processing!")
+  
+  ######################
+  #    Run HCMM-CNVs   #
+  ######################
+  
+  ploidy_option <- NA
+  if(paste(ploidy_path) == "1"){
+    ploidy_option <- "ploidy = \"1\""
+  }else{
+    ploidy_option <- "ploidy_path = ploidy_path"
+  }
+  paste(ploidy_option)
+  load(file = paste("Cov_matrix_chr_",chr_tmp,"_", filename, ".RData", sep=""))
+  
+  HCMM_CNVs(Cov_matrix = matrix_adj_Cov_rm_duplicated, n_cluster = n_cluster, 
+            bed_file_sorted = bed_file_sorted, sample_names = sample_names, 
+            filename = filename, paste(ploidy_option), chr = chr_tmp)
+  
+  ######################
+  # Run plot HCMM-CNVs #
+  ######################
+  
+  # don't actually need to run the R function provided, use the DNAcopy plot instead
+  
+  load(file = paste("CBS_chr_",chr,"_", filename, ".RData", sep=""))
+  
+  for(i in 1:length(names(CBS_all))){
+    png(paste("CNVs_chr_",chr,"_", names(CBS_all)[i], ".png", sep=""))
+    plot(CBS_all[[i]])
+    dev.off()
+  }
+  
+}else if(chr == "all"){
+  print("Analysing all chromosomes")
+  
+  bed <- read.table(bed_file, sep = "\t", header = T)
+  chr_list <- levels(as.factor(bed[,1]))
+  for(i in 1:length(chr_list)){
+    chr_tmp <- chr_list[i]
+    ######################
+    # Run pre-processing #
+    ######################
+    data_preproc(bed_file = bed_file, chr = chr_tmp, bam_dir = bam_dir, min_cov = min_cov, filename = filename)
+    
+    print("Finished pre-processing!")
+    
+    ######################
+    #    Run HCMM-CNVs   #
+    ######################
+    
+    ploidy_option <- NA
+    if(paste(ploidy_path) == "1"){
+      ploidy_option <- "ploidy = \"1\""
+    }else{
+      ploidy_option <- "ploidy_path = ploidy_path"
+    }
+    paste(ploidy_option)
+    load(file = paste("Cov_matrix_chr_", chr_tmp,"_", filename, ".RData", sep=""))
+    
+    HCMM_CNVs(Cov_matrix = matrix_adj_Cov_rm_duplicated, n_cluster = n_cluster, 
+              bed_file_sorted = bed_file_sorted, sample_names = sample_names, 
+              filename = filename, paste(ploidy_option), chr = chr_tmp)
+    
+    ######################
+    # Run plot HCMM-CNVs #
+    ######################
+    
+    # don't actually need to run the R function provided, use the DNAcopy plot instead
+    
+    load(file = paste("CBS_chr_",chr_tmp,"_", filename, ".RData", sep=""))
+    
+    for(i in 1:length(names(CBS_all))){
+      png(paste("CNVs_chr_",chr_tmp,"_", names(CBS_all)[i], ".png", sep=""))
+      plot(CBS_all[[i]])
+      dev.off()
+    }
+    
+  }
+  
 }
-paste(ploidy_option)
-load(file = paste("Cov_matrix_", filename, ".RData", sep=""))
 
-HCMM_CNVs(Cov_matrix = matrix_adj_Cov_rm_duplicated, n_cluster = n_cluster, 
-          bed_file_sorted = bed_file_sorted, sample_names = sample_names, 
-          filename = filename, paste(ploidy_option))
-
-######################
-# Run plot HCMM-CNVs #
-######################
-
-# don't actually need to run the R function provided, use the DNAcopy plot instead
-
-load(file = paste("CBS_", filename, ".RData", sep=""))
-
-for(i in 1:length(names(CBS_all))){
-  png(paste("CNVs_", names(CBS_all)[i], ".png", sep=""))
-  plot(CBS_all[[i]])
-  dev.off()
-}
